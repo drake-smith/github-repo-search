d3190786a55d10043ab87823bf980463801f299b
Drake Smith <drakesmithjr@gmail.com>
2022-03-20 15:03:11 -0400

    Create basic page container and header
    Reformat the component directory structure to split each
    component into its own directory. Create basic scaffolding of the
    main App page and main header 1.

---
+++ src/App/index.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import styled from 'styled-components';
+import { media } from '../shared/media';
+
+const { tablet, desktop } = media;
+
+const PageContainer = styled.div`
+  height: 100vh;
+  width: 100%;
+  padding: 1.5rem;
+
+  ${tablet`
+    padding: 2rem;
+  `}
+
+  ${desktop`
+    padding: 3rem;
+  `};
+`;
+
+const ContentContainer = styled.div`
+  max-width: 1280px;
+  margin: 0 auto;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Title = styled.h1`
+  font-size: 1.5rem;
+  font-weight: bold;
+  font-style: italic;
+  background-color: #ed1c24;
+  color: #ffffff;
+  display: inline-block;
+  padding: 0.5rem 1rem;
+
+  ${tablet`
+    font-size: 3rem;
+    padding: 0.5rem 1.5rem;
+  `}
+
+  ${desktop`
+    font-size: 4rem;
+  `};
+`;
+
+const App = () => {
+  return (
+    <PageContainer>
+      <ContentContainer>
+        <Title>GitHub Repo Search</Title>
+      </ContentContainer>
+    </PageContainer>
+  );
+};
+
+export default App;
